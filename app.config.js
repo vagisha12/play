9064be19965d859e13730386d7428cc797fb6036
naiefabdullah <90433828+naiefabdullah@users.noreply.github.com>
2021-12-08 01:35:42 -0500

    config

---
+++ app.config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfn2x9s6HWQnottH1qBdJMb3yZ4PYqU2k",
+    authDomain: "slack-alike-8d77f.firebaseapp.com",
+    databaseURL: "https://slack-alike-8d77f-default-rtdb.firebaseio.com",
+    projectId: "slack-alike-8d77f",
+    storageBucket: "slack-alike-8d77f.appspot.com",
+    messagingSenderId: "684397378206",
+    appId: "1:684397378206:web:0ea82415403eaa55bf998f",
+    measurementId: "G-M2E352B8MD"
+  }
+  
+  const cometChat = {
+    APP_ID: '199011bf0cdfca88',
+    AUTH_KEY: 'd7e868451c30aa23ec49a1f1fdbc9549371d863f',
+    REST_KEY: '398657ba6c9141a3e8a0d83908953f38e002eb87',
+    APP_REGION: 'us',
+  }
+  
+  export { firebaseConfig, cometChat }
